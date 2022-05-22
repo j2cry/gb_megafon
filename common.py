23c94ee235494289709039aad2b62b467f8a8a6b
avagadro <fragarie@yandex.com>
2022-05-23 00:52:19 +0300

    fit on a part of data after a given date (0.7777)
    default date is November 19, 2018

--- common.py
+++ common.py
@@ -4,3 +4,7 @@ import ctypes
 def trim_memory() -> int:
      libc = ctypes.CDLL("libc.so.6")
      return libc.malloc_trim(0)
+
+
+def select_and_sort(df, mask, by):
+    return df[mask].sort_values(by)
